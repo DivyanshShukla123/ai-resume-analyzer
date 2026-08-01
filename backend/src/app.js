cf3f49ab84d7c728c6f2b6f1d9b1d8c7570205e3
Divyansh Shukla <divyanshshukla23may@gmail.com>
2026-08-01 15:39:31 +0530

    redeploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,15 @@ import analysisRoutes from "./routes/analysis.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(
+  cors({
+    origin: [
+      "https://ai-resume-analyzer-xi-eosin.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
